46eeb32915c0eec0384731294e361ee3af239a58
kuldeep.gupta <kuldeep.gupta@zycus.com>
2019-03-25 21:33:05 +0530

    Basic Skeleton

--- src/main/java/com/zycus/validator/io/XlslReader.java
+++ src/main/java/com/zycus/validator/io/XlslReader.java
@@ -1,11 +1,29 @@
 package com.zycus.validator.io;
 
+import com.zycus.validator.core.BasicValidationService;
+import com.zycus.validator.core.ValidationService;
+import org.dhatim.fastexcel.reader.ReadableWorkbook;
+import org.dhatim.fastexcel.reader.Row;
+import org.dhatim.fastexcel.reader.Sheet;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.stream.Stream;
+
 public class XlslReader {
 
-    public void readRows() {
+    private ValidationService validationService = new BasicValidationService();
 
+    public void readRows(String filePath) throws IOException {
 
+        try (InputStream is = new FileInputStream(filePath); ReadableWorkbook wb = new ReadableWorkbook(is)) {
 
+            Sheet sheet = wb.getFirstSheet();
+            try (Stream<Row> rows = sheet.openStream()) {
+
+            }
+        }
     }
 
 }
